13ec40033bed1dd7af6ae1cbaaacc9ed505382a2
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2015-05-26 06:36:41 +0200

    0.06

--- lib/LCS.pm
+++ lib/LCS.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use 5.006;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Data::Dumper;
 
--- lib/LCS/Tiny.pm
+++ lib/LCS/Tiny.pm
@@ -3,7 +3,7 @@ package LCS::Tiny;
 use 5.006;
 use strict;
 use warnings;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 #use utf8;
 
 sub new {
